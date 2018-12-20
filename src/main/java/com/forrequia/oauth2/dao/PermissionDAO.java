90d49c4c91ab836ca8153851974ed88a68b1711d
forrequia <orrequia8@gmail.com>
2018-12-20 17:24:29 +0100

    Eliminado ficheros no necesario debido a la eliminación de los Roles en el servidor de autenticación.

--- src/main/java/com/forrequia/oauth2/dao/PermissionDAO.java
+++
@@ -1,9 +0,0 @@
-package com.forrequia.oauth2.dao;
-
-import com.forrequia.oauth2.model.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PermissionDAO extends JpaRepository<Permission, Long> {
-}
--- src/main/java/com/forrequia/oauth2/dao/RoleDAO.java
+++
@@ -1,9 +0,0 @@
-package com.forrequia.oauth2.dao;
-
-import com.forrequia.oauth2.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleDAO extends JpaRepository<Role, Long> {
-}
